6f255b0af7c827a1a211efc088c53a5e3746dde8
nm <nagamocha@gmail.com>
2024-07-04 01:05:33 +0300

    Add search locations by resident characters

--- src/main.js
+++ src/main.js
@@ -6,39 +6,67 @@ const main = async () => {
     dbPath: "app.db",
   };
   const db = await getDB(config);
-  const results = await searchLocationsByName(db, "Pluto");
+  let results = await searchLocationsByResidentCharacters(db, "Evil");
+  for (let r of results) {
+    console.log(r);
+  }
+  console.log("--------------");
+  results = await searchLocationsByName(db, "Rick");
   for (let r of results) {
     console.log(r);
   }
 };
+const genSearchResultsSQL = (matchingLocationsSQL) => `
+  with matches as (
+    ${matchingLocationsSQL}
+  ), residents as (
+      select
+          l.id as location_id,
+          list(
+            {'id': c.id, 'name': c.name, 'status': c.status, 'image': c.image}
+          ) as residents
+      from matches l
+      join character c on l.id = c.last_known_location
+      group by l.id
+  )
+  select
+      l.id as location_id,
+      l.name as location_name,
+      l.type as location_type,
+      case when len(r.residents) >= 1 then [1]
+      else [] end as residents
+  from matches l
+  left join residents r on l.id = r.location_id
+  order by l.score desc
+`;
+
+const searchLocationsByResidentCharacters = async (db, searchTerm) => {
+  const matchingLocationsSQL = `
+        select l.id, l.name, l.type, score
+        from (
+          select
+             last_known_location as location_id,
+             max(fts_main_character.match_bm25(id, ?)) as score
+          from character
+          group by location_id
+        ) as t
+        join location l on t.location_id = l.id
+        where score is not null
+  `;
+  const sql = genSearchResultsSQL(matchingLocationsSQL);
+  const rows = await db.all(sql, searchTerm);
+  return rows;
+};
 
 const searchLocationsByName = async (db, searchTerm) => {
-  const sql = `
-    with matching_locations as (
+  const matchingLocationsSQL = `
       select
           id, name, type,
           fts_main_location.match_bm25(id, ?) as score,
       from location
       where score is not null
-    ), residents as (
-        select
-            l.id as location_id,
-            list(
-              {'id': c.id, 'name': c.name, 'status': c.status, 'image': c.image}
-            ) as residents
-        from matching_locations l
-        join character c on l.id = c.last_known_location
-        group by l.id
-    )
-    select
-        l.id as location_id,
-        l.name as location_name,
-        l.type as location_type,
-        case when len(r.residents) >= 1 then r.residents
-        else [] end as residents
-    from matching_locations l
-    left join residents r on l.id = r.location_id
   `;
+  const sql = genSearchResultsSQL(matchingLocationsSQL);
   const rows = await db.all(sql, searchTerm);
   return rows;
 };
