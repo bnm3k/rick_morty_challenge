d6806f915f8562fb50825de7c91f20eba84f007d
nm <nagamocha@gmail.com>
2024-07-04 01:30:32 +0300

    Add search locations by episode name

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ const main = async () => {
     dbPath: "app.db",
   };
   const db = await getDB(config);
-  let results = await searchLocationsByResidentCharacters(db, "Evil");
+  let results;
+  results = await searchLocationsByResidentCharacters(db, "Evil");
   for (let r of results) {
     console.log(r);
   }
@@ -15,8 +16,15 @@ const main = async () => {
   for (let r of results) {
     console.log(r);
   }
+  console.log("--------------");
+  results = await searchLocationsByEpisodeName(db, "Morty");
+  for (let r of results) {
+    console.log(r);
+  }
 };
-const genSearchResultsSQL = (matchingLocationsSQL) => `
+
+const genSearchFn = (matchingLocationsSQL) => {
+  const sql = `
   with matches as (
     ${matchingLocationsSQL}
   ), residents as (
@@ -33,43 +41,57 @@ const genSearchResultsSQL = (matchingLocationsSQL) => `
       l.id as location_id,
       l.name as location_name,
       l.type as location_type,
-      case when len(r.residents) >= 1 then [1]
+      case when len(r.residents) >= 1 then r.residents
       else [] end as residents
   from matches l
   left join residents r on l.id = r.location_id
   order by l.score desc
-`;
+ `;
 
-const searchLocationsByResidentCharacters = async (db, searchTerm) => {
-  const matchingLocationsSQL = `
-        select l.id, l.name, l.type, score
-        from (
-          select
-             last_known_location as location_id,
-             max(fts_main_character.match_bm25(id, ?)) as score
-          from character
-          group by location_id
-        ) as t
-        join location l on t.location_id = l.id
-        where score is not null
-  `;
-  const sql = genSearchResultsSQL(matchingLocationsSQL);
-  const rows = await db.all(sql, searchTerm);
-  return rows;
+  return async (db, searchTerm) => {
+    const rows = await db.all(sql, searchTerm);
+    return rows;
+  };
 };
 
-const searchLocationsByName = async (db, searchTerm) => {
-  const matchingLocationsSQL = `
+const searchLocationsByEpisodeName = genSearchFn(`
+    select l.id, l.name, l.type, score
+    from (
+        select
+            c.last_known_location as location_id,
+            max(fts_main_episode.match_bm25(e.id, ?)) as score
+        from episode e
+        join episode_to_character ec on e.id = ec.episode_id
+        join character c on ec.character_id = c.id
+        group by location_id
+    ) as t
+    join location l on t.location_id = l.id
+    where score is not null
+  `);
+
+const searchLocationsByResidentCharacters = genSearchFn(
+  `
+    select l.id, l.name, l.type, score
+    from (
+      select
+         last_known_location as location_id,
+         max(fts_main_character.match_bm25(id, ?)) as score
+      from character
+      group by location_id
+    ) as t
+    join location l on t.location_id = l.id
+    where score is not null
+  `
+);
+const searchLocationsByName = genSearchFn(
+  `
       select
           id, name, type,
           fts_main_location.match_bm25(id, ?) as score,
       from location
       where score is not null
-  `;
-  const sql = genSearchResultsSQL(matchingLocationsSQL);
-  const rows = await db.all(sql, searchTerm);
-  return rows;
-};
+  `
+);
 
 // retrieve a list of locations (name and type), along with residents of that
 // location and their status.
