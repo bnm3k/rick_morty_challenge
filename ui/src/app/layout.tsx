f25fc0155daf81b25ae3b332ba72b02ad065ce67
nm <nagamocha@gmail.com>
2024-08-09 17:38:00 +0300

    Init ui stuff

---
+++ ui/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Rick & Morty Locations Search",
+  description: "Search locations that appeared in the Rick & Morty show",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/concrete.css/3.0.0/concrete.min.css"
+        />
+
+        <meta name="theme-color" content="#ffffff" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
